1cd6596f5770682809c5d8069d6fd3663dd3db9b
Richard Böhme <richard.boehme1999@gmail.com>
2021-10-20 16:50:44 +0200

    Remove unused ComplexType enum

--- TestingMSAGL/DataLinker/NodeComplex.cs
+++ TestingMSAGL/DataLinker/NodeComplex.cs
@@ -28,9 +28,6 @@ namespace TestingMSAGL.DataLinker
         /// </summary>
         internal CompositeComplex Composite { get; }
 
-        // todo yet to decide if needed
-        protected ComplexType ComplexType { get; set; }
-
         /// <summary>
         ///     Pass node id to Extern
         /// </summary>
@@ -85,12 +82,6 @@ namespace TestingMSAGL.DataLinker
         //    // TODO Maybe another member list is required here. . . 
         //    //TODO maybe some black magic for constraints
         //}
-
-        private void SetFormat(ComplexType format)
-        {
-            ComplexType = format;
-        }
-
 
         /// <summary>
         ///     add a new Node to Complex node and return true on success
