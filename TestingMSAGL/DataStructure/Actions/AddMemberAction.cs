2fdb96781ea9409ca90730626b8ae293ce08ecab
Richard Böhme <richard.boehme1999@gmail.com>
2021-12-11 11:28:05 +0100

    Add AddMemberAction and RemoveMemberAction

---
+++ TestingMSAGL/DataStructure/Actions/AddMemberAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestingMSAGL.DataStructure.Actions
+{
+    public class AddMemberAction : ICompositeAction
+    {
+        public string Error { get; private set; }
+
+        private readonly CompositeComplex _parent;
+        private readonly Composite _child;
+
+        public AddMemberAction(CompositeComplex parent, Composite child)
+        {
+            _parent = parent;
+            _child = child;
+        }
+        
+        public bool Perform()
+        {
+            var result = _parent.AddMember(_child);
+            if (!result)
+                Error = string.Join("\n", _parent.ConsumeErrors());
+            return result;
+        }
+
+        public void Rollback()
+        {
+            _parent.RemoveMember(_child);
+        }
+    }
+}
---
+++ TestingMSAGL/DataStructure/Actions/RemoveMemberAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestingMSAGL.DataStructure.Actions
+{
+    public class RemoveMemberAction : ICompositeAction
+    {
+        public string Error { get; private set; }
+
+        private readonly CompositeComplex _parent;
+        private readonly Composite _child;
+
+        public RemoveMemberAction(CompositeComplex parent, Composite child)
+        {
+            _parent = parent;
+            _child = child;
+        }
+        
+        public bool Perform()
+        {
+            var result = _parent.RemoveMember(_child);
+            if (!result)
+                Error = string.Join("\n", _parent.ConsumeErrors());
+            return result;
+        }
+
+        public void Rollback()
+        {
+            _parent.AddMember(_child);
+        }
+    }
+}
